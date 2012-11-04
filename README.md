2bf3c924de52a3b101dbd831e932cf9bd20c034d
Brad Chen <brad5620732@hotmail.com>
2012-11-03 23:14:32 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,72 @@
+## Convert emoji into HTML images for phpBB3
+
+This is a work in progress MOD for phpBB3 that converts iOS emojis into HTML images. This MOD is based on dpaanlka’s work published at: [https://www.phpbb.com/community/viewtopic.php?f=70&amp;t=2111155](https://www.phpbb.com/community/viewtopic.php?f=70&amp;t=2111155).
+
+Emoji images are mostly from github/gemoji. Any_SoftbankSMS.txt is from [Apple](http://opensource.apple.com/source/ICU/ICU-461.13/icuSources/data/translit/Any_SoftbankSMS.txt?txt).
+
+### Installation
+
+#### Update Database Schema to Support 4-Byte UTF-8
+
+Instructions here are for MySQL only. To store 4-byte utf-8, MySQL version 5.5 and above is required. Change collation of phpbb_posts.post_text to utf8mb4_unicode_ci. Repeat this step for any other places, such as phpbb_forums.forum_desc, that may possibly hold emoji.
+
+Open `phpBB/includes/db/mysqli.php`, change:
+
+```php
+			@mysqli_query($this->db_connect_id, "SET NAMES 'utf8'");
+```
+
+to:
+
+```php
+			@mysqli_query($this->db_connect_id, "SET NAMES 'utf8mb4'");
+```
+
+#### Copy Files
+
+* Copy `images/emoji` into `phpBB/images`
+* Copy `includes/covert_emoji.php` into `phpBB/includes`
+
+#### Add Function Call
+
+Open `phpBB/includes/functions_content.php`, change:
+
+```php
+function smiley_text($text, $force_option = false)
+{
+	global $config, $user, $phpbb_root_path;
+```
+
+to
+
+```php
+function smiley_text($text, $force_option = false)
+{
+	global $config, $user, $phpbb_root_path;
+
+	include_once dirname(__FILE__) . '/convert_emoji.php';
+	$text = convert_emoji($text);
+```
+
+### License
+
+All images and Any_SoftbankSMS.txt:
+
+Copyright (c) 2012 Apple Inc. All rights reserved.
+
+The rest:
+
+```
+  Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+```
+
---
+++ script/generate_map.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * This is used to generate contents in convert_emoji.php. Aside from it, it has
+ * no other use.
+ */
+
+// load conversion table
+$raw = file_get_contents('Any_SoftbankSMS.txt');
+$map = array();
+$lines = split(PHP_EOL, $raw);
+foreach ($lines as $line) {
+  $line = trim(preg_replace('/#.*/', '', $line));
+  $line = trim(preg_replace('/;$/', '', $line));
+  if (!$line) {
+    continue;
+  }
+
+  $tokens = preg_split('/ [^\\\\]+/', $line);
+  if (count($tokens) != 2) {
+    continue;
+  }
+
+  $key = str_replace(array('\U', '\u'), '', $tokens[0]);
+  $map[$key] = substr($tokens[1], 2);
+}
+$content = file_get_contents('symbols.txt');
+$count = 0;
+for ($i = 0; $i < strlen($content); ) {
+  $length = 3;
+  if (substr($content, $i, 1) == "\xc2") {
+    $length = 2;
+  } else if (substr($content, $i, 1) == "\xf0") {
+    $length = 4;
+
+    // special case regional flag
+    if (substr($content, $i, 3) == "\xf0\x9f\x87") {
+      $length = 8;
+    }
+  } else if (substr($content, $i + 1, 3) == "\xe2\x83\xa3") {
+    // another special case
+    $length = 4;
+  }
+
+  $char = substr($content, $i, $length);
+  $utf32 = strtoupper(utf8_to_utf32_str($char));
+
+  // special cases
+  if (substr($content, $i, 1) == "\xc2") {
+    $utf32 = substr($utf32, 4, 4);
+  } else if (substr($content, $i + 1, 3) == "\xe2\x83\xa3") {
+    $utf32 = substr($utf32, 4, 4) . substr($utf32, 12, 4);
+  }
+
+  $zeroes_trimmed = ltrim($utf32, '0');
+  $filename = strtolower($zeroes_trimmed);
+  if (isset($map[$utf32])) {
+    $filename = strtolower($map[$utf32]);
+  } else if (isset($map[$zeroes_trimmed])) {
+    $filename = strtolower($map[$zeroes_trimmed]);
+  }
+
+  $filename .= '.png';
+  if (!file_exists('unicode-emoji-images/' . $filename)) {
+    echo 'File not found: #' . $count . ' ' . $utf32 . ' ' . $char . ' '
+      . utf8_to_str($char) . ' ' . $filename  . PHP_EOL;
+    die();
+  }
+
+  /*
+  echo "\t\t'";
+  echo 'symbols/' . $filename;
+  echo "'," . PHP_EOL;
+  copy('unicode-emoji-images/' . $filename, 'symbols/' . $filename);*/
+
+  echo "\t\t\"";
+  echo utf8_to_str($char);
+  echo '",' . PHP_EOL;
+
+  $i += $length;
+  $count++;
+}
+
+function utf8_to_str($utf8) {
+  $result = '';
+  for ($i = 0; $i < strlen($utf8); $i++) {
+    $result .= sprintf('\x%x', ord(substr($utf8, $i, 1)));
+  }
+  return $result;
+}
+
+function utf8_to_utf32_str($utf8) {
+  $utf32 = mb_convert_encoding($utf8, 'utf-32', 'utf-8');
+  $result = '';
+  for ($i = 0; $i < strlen($utf32); $i++) {
+    $result .= sprintf('%02x', ord(substr($utf32, $i, 1)));
+  }
+  return $result;
+}
+
+echo 'Count: ' . $count . PHP_EOL;
+
